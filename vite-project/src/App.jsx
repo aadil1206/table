7aebf7702e0b8667d7802836fa051179933c38ab
Md Adil Ashrafi <105966902+aadil1206@users.noreply.github.com>
2024-04-18 13:41:29 +0530

    change

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import Table from './components/Table'
+import "./App.css";
+import Table from "./components/Table";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div  className='App-main'>
-   <Table/>
+    <div className="App-main">
+      <Table />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
